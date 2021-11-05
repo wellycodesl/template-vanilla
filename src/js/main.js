517eacb5aedcd1c4cb7f5f3e526dd729d0d2781f
Edu Iglesias <eiglesiash@gmail.com>
2021-11-05 13:24:33 +0100

    Ajustes de la plantilla

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+const initFrameworkWelly = ()=> {
+    console.log('Js Welly Ready!!!')
+    document.getElementById('welly').innerHTML = 'Js Welly Ready!!!'
+
+    // Do Something
+}
+
+
+window.addEventListener('load', initFrameworkWelly)
